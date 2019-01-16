851130a04a39dd3d22172d685f29221cbdf751c8
tamamu <tamamu.1r1s@gmail.com>
2019-01-17 01:51:30 +0900

    Render points and rectangle

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -16,6 +16,7 @@ const vertexShaderSource: &str = r#"
     #version 330 core
     layout (location = 0) in vec3 aPos;
     void main() {
+        gl_PointSize = 10.0;
         gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
     }
 "#;
@@ -27,6 +28,9 @@ const fragmentShaderSource: &str = r#"
         FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     }
 "#;
+
+type Vertices1 = [f32; 9];
+type Vertices2 = [f32;12];
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -43,11 +47,25 @@ fn main() {
     window.set_framebuffer_size_polling(true);
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-    let (shaderProgram, VAO) = create_program_and_vao();
+    unsafe { gl::Enable(gl::PROGRAM_POINT_SIZE); }
 
     let mut delta_time: f32;
     let mut last_frame: f32 = 0.0;
+
+    let mut tri_vertices: Vertices1 = [
+        -0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.0,  0.5, 0.0
+    ];
+    let mut rect_vertices: Vertices2 = [
+        -0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.5,  0.5, 0.0,
+        -0.5,  0.5, 0.0
+    ];
+
+    let (shaderProgram1, VAO1, VBO1) = create_program_and_vao(&tri_vertices, gl::DYNAMIC_DRAW);
+    let (shaderProgram2, VAO2, VBO2) = create_program_and_vao(&rect_vertices, gl::STATIC_DRAW);
 
     while !window.should_close() {
         let current_frame = glfw.get_time() as f32;
@@ -60,14 +78,50 @@ fn main() {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::UseProgram(shaderProgram);
-            gl::BindVertexArray(VAO);
+            gl::UseProgram(shaderProgram1);
+
+            let (x1, y1) = calc_rectangle_position(last_frame);
+            let (x2, y2) = calc_rectangle_position(3.14 + last_frame * 0.64);
+            let (x3, y3) = calc_rectangle_position(1.41 + last_frame * 1.28);
+            tri_vertices[0] = x1;
+            tri_vertices[1] = y1;
+            tri_vertices[3] = x2;
+            tri_vertices[4] = y2;
+            tri_vertices[6] = x3;
+            tri_vertices[7] = y3;
+            gl::BindBuffer(gl::ARRAY_BUFFER, VBO1);
+            gl::BufferSubData(gl::ARRAY_BUFFER,
+                              0,
+                              (tri_vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                              &tri_vertices[0] as *const f32 as *const c_void);
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+
+
+            gl::BindVertexArray(VAO1);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawArrays(gl::POINTS, 0, 3);
+
+            gl::UseProgram(shaderProgram2);
+            gl::BindVertexArray(VAO2);
+            gl::DrawArrays(gl::LINE_LOOP, 0, 4);
         }
 
         window.swap_buffers();
 
         glfw.poll_events();
+    }
+}
+
+fn calc_rectangle_position(t: f32) -> (f32, f32) {
+    let u = t % 4.0;
+    if u > 3.0 {
+        (-0.5, 0.5 - (u - 3.0))
+    } else if u > 2.0 {
+        (0.5 - (u - 2.0), 0.5)
+    } else if u > 1.0 {
+        (0.5, -0.5 + (u - 1.0))
+    } else {
+        (-0.5 + u, -0.5)
     }
 }
 
@@ -84,7 +138,8 @@ fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::Windo
     }
 }
 
-fn create_program_and_vao() -> (u32, u32) {
+
+fn create_program_and_vao(vertices: &[f32], draw_type: GLenum) -> (u32, u32, u32) {
     unsafe {
         let vertexShader = gl::CreateShader(gl::VERTEX_SHADER);
         let c_str_vert = CString::new(vertexShaderSource.as_bytes()).unwrap();
@@ -127,11 +182,21 @@ fn create_program_and_vao() -> (u32, u32) {
         gl::DeleteShader(fragmentShader);
 
         // setup vertex
-        let vertices: [f32; 9] = [
+        /*
+        let mut tri_vertices: Vertices1 = [
             -0.5, -0.5, 0.0,
              0.5, -0.5, 0.0,
              0.0,  0.5, 0.0
         ];
+        */
+        /*
+        let mut rect_vertices: Vertices2 = [
+            -0.5, -0.5, 0.0,
+             0.5, -0.5, 0.0,
+             0.5,  0.5, 0.0,
+            -0.5,  0.5, 0.0
+        ];
+        */
         let (mut VBO, mut VAO) = (0, 0);
         gl::GenVertexArrays(1, &mut VAO);
         gl::GenBuffers(1, &mut VBO);
@@ -142,9 +207,10 @@ fn create_program_and_vao() -> (u32, u32) {
         gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
         gl::BufferData(gl::ARRAY_BUFFER,
                        (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                       &vertices[0] as *const f32 as *const c_void,
-                       gl::STATIC_DRAW);
+                       vertices.as_ptr() as *const c_void,
+                       draw_type);
 
+        // set location 0 of vertex shader to 3 floats
         gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
         gl::EnableVertexAttribArray(0);
 
@@ -152,6 +218,6 @@ fn create_program_and_vao() -> (u32, u32) {
 
         gl::BindVertexArray(0);
 
-        (shaderProgram, VAO)
+        (shaderProgram, VAO, VBO)
     }
 }
