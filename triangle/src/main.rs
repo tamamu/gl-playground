8612313f250b5815e479e5ab6815f1000f70636f
tamamu <tamamu.1r1s@gmail.com>
2019-01-16 23:48:59 +0900

    Use GL

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -2,27 +2,42 @@
 extern crate glfw;
 use glfw::{Action, Context, Key};
 
+extern crate gl;
+use std::sync::mpsc::Receiver;
+
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
     let (mut window, events) = glfw.create_window(800, 600, "TRIANGLE", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.make_current();
     window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
     while !window.should_close() {
+        process_events(&mut window, &events);
         window.swap_buffers();
 
         glfw.poll_events();
-        for (i, event) in glfw::flush_messages(&events) {
-            println!("{:?}, {:?}", i, event);
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
-                },
-                _ => {},
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (i, event) in glfw::flush_messages(&events) {
+        println!("{:?}, {:?}", i, event);
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                unsafe { gl::Viewport(0, 0, width, height) }
             }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            _ => {}
         }
     }
 }
