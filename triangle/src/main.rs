b4fb392be619f309d239b1937b1d3590f7c73fc2
tamamu <tamamu.1r1s@gmail.com>
2019-01-16 23:30:50 +0900

    Show window

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -1,3 +1,28 @@
+
+extern crate glfw;
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw.create_window(800, 600, "TRIANGLE", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+
+        glfw.poll_events();
+        for (i, event) in glfw::flush_messages(&events) {
+            println!("{:?}, {:?}", i, event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                },
+                _ => {},
+            }
+        }
+    }
 }
